247cbae1d008987c665ea697fd6a9de31e09e5e7
DefDumbCode <dimas07072007@gmail.com>
2025-09-29 18:33:05 +0300

    Создано меню

--- ISIP523-Sidorov/Program.cs
+++ ISIP523-Sidorov/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿List<char> punct = new List<char>() { '.', ',', '!', '?', '"', '-', ':', ';' };
+List<char> cons = new List<char>() { 'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ' };
+List<char> vowels = new List<char>() { 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я' };
+List<string> Texts = new List<string>();
+List<Dictionary<char, int>> Stats = new List<Dictionary<char, int>>();
+
+string input = "1";
+while (input != "6")
+{
+    Console.WriteLine("\n/__________МЕНЮ___________/");
+    Console.WriteLine("1. Добавить новый текст.");
+    Console.WriteLine("2. Просмотр текста.");
+    Console.WriteLine("3. Статистика по тексту.");
+    Console.WriteLine("4. Выход.");
+    input = Console.ReadLine();
+    switch (input)
+    {
+        case "1":
+            TextAdd();
+            break;
+        case "2":
+            if (Texts.Count > 0)
+            {
+                TextView();
+            }
+            else
+            {
+                Console.WriteLine("Сначала введите текст!");
+            }
+            break;
+        case "3":
+            if (Texts.Count > 0)
+            {
+                TextStat();
+            }
+            else
+            {
+                Console.WriteLine("Сначала введите текст!");
+            }
+            break;
+        case "4":
+            break;
+        default:
+            Console.WriteLine("Неправильная команда!");
+            break;
+    }
+}
+  
