4535abd9d3e1c9c5b625bace1605689ea076b76f
DefDumbCode <dimas07072007@gmail.com>
2025-09-29 18:35:04 +0300

    Добавлен вывод статистики

--- ISIP523-Sidorov/Program.cs
+++ ISIP523-Sidorov/Program.cs
@@ -71,5 +71,107 @@ void TextView()
     Console.WriteLine(Texts[id]);
 }
 
+void TextStat()
+{
+    Console.Write("Введите ID текста: ");
+    int id = int.Parse(Console.ReadLine()) - 1;
+    while (id < 0 || id > Texts.Count - 1)
+    {
+        Console.WriteLine("Неправильный индекс!");
+        Console.Write("Введите ID текста: ");
+        id = int.Parse(Console.ReadLine()) - 1;
+    }
+    Console.WriteLine("\n___СТАТИСТИКА___");
+    string[] words = Texts[id].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+    Dictionary<char, int> stat = new Dictionary<char, int>()
+    {
+    { 'а', 0 }, { 'б', 0 }, { 'в', 0 },
+    { 'г', 0 }, { 'д', 0 }, { 'е', 0 },
+    { 'ё', 0 }, { 'ж', 0 }, { 'з', 0 },
+    { 'и', 0 }, { 'й', 0 }, { 'к', 0 },
+    { 'л', 0 }, { 'м', 0 }, { 'н', 0 },
+    { 'о', 0 }, { 'п', 0 }, { 'р', 0 },
+    { 'с', 0 }, { 'т', 0 }, { 'у', 0 },
+    { 'ф', 0 }, { 'х', 0 }, { 'ц', 0 },
+    { 'ч', 0 }, { 'ш', 0 }, { 'щ', 0 },
+    { 'ъ', 0 }, { 'ы', 0 }, { 'ь', 0 },
+    { 'э', 0 }, { 'ю', 0 }, { 'я', 0 },
+    };
+
+
+    for (int i = 0; i < words.Length; i++)
+    {
+        for (int j = 0; j < words[i].Length; j++)
+        {
+            if (punct.Contains(words[i][j]))
+            {
+                words[i] = words[i].Remove(j);
+            }
+        }
+    }
+
+
+    string shortest = words[0];
+    for (int i = 1; i < words.Length; i++)
+    {
+        if (words[i].Length < shortest.Length && words[i] != "")
+        {
+            shortest = words[i];
+        }
+    }
+    Console.WriteLine($"Самое короткое слово: {shortest}");
+
+    string longest = words[0];
+    for (int i = 0; i < words.Length - 1; i++)
+    {
+        if (words[i].Length > longest.Length)
+        {
+            longest = words[i];
+        }
+    }
+    Console.WriteLine($"Самое длинное слово: {longest}");
+
+    int sent = 0;
+    foreach (char s in Texts[id])
+    {
+        if (s == '.' || s == '!' || s == '?')
+        {
+            sent++;
+        }
+    }
+    Console.WriteLine($"Предложений в тексте: {sent}");
+
+
+    int cons_count = 0;
+    int volwes_count = 0;
+    foreach (string word in words)
+    {
+        foreach (char s in word.ToLower())
+        {
+            if (cons.Contains(s))
+            {
+                cons_count++;
+            }
+            else if (vowels.Contains(s))
+            {
+                volwes_count++;
+            }
+            stat[s]++;
+        }
+    }
+    Console.WriteLine($"Кол-во согласных в тексте: {cons_count}");
+    Console.WriteLine($"Кол-во гласных в тексте: {volwes_count}");
+
+
+    Console.WriteLine("___СТАТИСТИКА ПО БУКВАМ___");
+    foreach (var (key, val) in stat)
+    {
+        double precent = Convert.ToDouble(val) / Texts[id].Length * 100;
+        Console.WriteLine($"{key}: {val} ({Math.Round(precent, 2)}%)");
+    }
+    Stats.Add(stat);
+}
+
 
 
