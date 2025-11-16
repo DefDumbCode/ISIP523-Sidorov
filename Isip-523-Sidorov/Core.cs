147c58375e527a6eed9f0ace1e75b18bddffc0ea
DefDumbCode <dimas07072007@gmail.com>
2025-11-16 17:07:15 +0300

    Реализована интеграция БД

---
+++ Isip-523-Sidorov/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isip_523_Sidorov
+{
+    internal class Core
+    {
+        public static STOEntities2 Context = new STOEntities2();
+    }
+}
---
+++ Isip-523-Sidorov/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isip_523_Sidorov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Details> details = new List<Details>();
+
+            Console.WriteLine("1. Начать игру\n" +
+                "2. Выход");
+            string input = Console.ReadLine();
+            while (input != "1" || input != "2")
+            {
+                Console.WriteLine("Некорректный ввод!");
+                Console.Write("Повторный ввод: ");
+                input = Console.ReadLine();
+            }
+
+            switch (input)
+            {
+                case "1":
+                    //Game();
+                    break;
+                case "2":
+                    break;
+                default:
+                    break;
+            }
+
+
+
+
+            int IntInput()
+            {
+                if (int.TryParse(Console.ReadLine(), out int output))
+                {
+                    return output;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод!");
+                    Console.Write("Повторный ввод: ");
+                    return IntInput();
+                }
+            }
+
+
+            void Game()
+            {
+
+            }
+
+        }
+    }
+}
