39dc83775618543f36398107aa27735f277c340d
DefDumbCode <dimas07072007@gmail.com>
2025-11-17 09:59:13 +0300

    Реализовано создание нового клиента

--- Isip-523-Sidorov/Program.cs
+++ Isip-523-Sidorov/Program.cs
@@ -54,8 +54,80 @@ namespace Isip_523_Sidorov
                 }
             }
 
+            void Game(Player player)
+            {
+                List<Wherehorse> players_wharehouse = wharehouses.Where(d => d.PlayerID == player.PlayerID).ToList();
+                while (player.Balance >= 0)
+                {
+                    Car car = new Car();
+                    car.NewCar(details);
+                    List<Wherehorse> players_detail = new List<Wherehorse>();
+                    Console.WriteLine("Сломанные детали");
+                    foreach (var item in car.Broken_details)
+                    {
+                        players_detail.Add(players_wharehouse.First(d => d.DetailID == item.DetailID));
+                        foreach (var detail in players_detail)
+                        {
+                            Console.WriteLine($"{item.DetailName} (У Вас: {detail.Amount})");
+                        }
+                    }
 
-            
+
+                    Console.WriteLine("Что делать?\n" +
+                        "1. Чинить.\n" +
+                        "2. Отказать.\n" +
+                        "3. Купить детали.");
+
+                    string action = Console.ReadLine();
+                    while (action != "1" && action != "2" && action != "3")
+                    {
+                        Console.WriteLine("Некорректный ввод!");
+                        Console.Write("Повторный ввод: ");
+                        action = Console.ReadLine();
+                    }
+
+                    switch (action)
+                    {
+                        case "1":
+                            bool can_serve = true;
+                            foreach (var item in players_detail)
+                            {
+                                if (item.Amount == 0)
+                                {
+                                    can_serve = false;
+                                }
+                            }
+
+                            if (can_serve == true)
+                            {
+                                foreach (var item in players_detail)
+                                {
+                                    players_wharehouse.First(d => d.DetailID == item.DetailID).Amount--;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("У Вас не оказалось нужной детали!\n" +
+                                    "Клиет недоволен! (-250 зол.)");
+                                player.Balance -= 200;
+                            }
+
+                            break;
+                        case "2":
+                            Console.WriteLine("Клиент недоволен! (-200 зол.)");
+                            player.Balance -= 200;
+                            break;
+                        case "3":
+                            //BuyDetails();
+                            break;
+                        default:
+                            //BuyDetails();
+                            break;
+                    }
+
+                }
+            }
+
 
             void NewPlayer(out Player player)
             {
