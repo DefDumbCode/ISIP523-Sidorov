3d239f175e498483b99ad05b95e5a569f799bb3c
DefDmbCode <dimas07072007@gmail.com>
2026-01-12 12:07:40 +0300

    Реализован вывод имеюихся пицц

---
+++ Pooomnite/Pages/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pooomnite.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    /// 
+
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+
+    }
+
+    public partial class MainPage : Page
+    {
+        public List<Pizza> PizzasList = new List<Pizza>
+        {
+            new Pizza
+            {
+                Name = "Маргарита",
+                Description = "Крутая пицца Маргарита",
+                Price = 400
+            },
+
+            new Pizza
+            {
+                Name = "4 Сыра",
+                Description = "Иди на все 4 стороны с этой пиццой",
+                Price = 450
+            },
+
+            new Pizza
+            {
+                Name = "Охотничья",
+                Description = "Нет, она не сделает тебя охотником",
+                Price = 500
+            },
+
+            new Pizza
+            {
+                Name = "Пирог",
+                Description = "Господь не будет так милостив с тобой...",
+                Price = 400
+            },
+
+            new Pizza
+            {
+                Name = "Гавайская",
+                Description = "Видимо, у тебя нет друзей",
+                Price = 400
+            }
+        };  
+        public MainPage()
+        {
+            InitializeComponent();
+
+            PizzasListBox.ItemsSource = PizzasList;
+        }
+
+        
+    }
+
+}
