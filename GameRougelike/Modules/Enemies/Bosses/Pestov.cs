03318671e0d767f964e195efb7ed7f4d10fcb669
DefDumbCode <dimas07072007@gmail.com>
2026-04-09 13:01:23 +0300

    FFFF

---
+++ GameRougelike/Modules/Enemies/Bosses/Pestov.cs
@@ -0,0 +1,39 @@
+﻿using ISIP523_Sidorov.Modules.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP523_Sidorov.Modules.Enemies.Bosses
+{
+    internal class Pestov : Sekleton
+    {
+        public double Freeze;
+        public Pestov()
+        {
+            HP = Math.Round(HP * 1.3);
+            ATK = Math.Round(ATK * 1.8);
+            DEF = Math.Round(DEF * 0.6);
+            Freeze = 0.5;
+        }
+
+        public override void Attack(Hero Player, bool def, bool dodge)
+        {
+            Random rand = new Random();
+            if (dodge == false)
+            {
+                Player.GetDamage(ATK);
+                if (rand.NextDouble() > 1 - Freeze)
+                {
+                    Console.WriteLine("Вас заморозили! Вы пропускаете ход.");
+                    Attack(Player, true, false);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы успешно уклонились от атаки");
+            }
+        }
+    }
+}
---
+++ GameRougelike/Pages/GamePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ISIP523_Sidorov.Modules.Entities;
+using ISIP523_Sidorov.Modules.Equipment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameRougelike.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для GamePage.xaml
+    /// </summary>
+    public partial class GamePage : Page
+    {
+        public static Weapon weapon = new Weapon();
+        public static Armor armor = new Armor();
+        public static Hero hero = new Hero(weapon, armor);
+        public GamePage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void AtkBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BlockBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
